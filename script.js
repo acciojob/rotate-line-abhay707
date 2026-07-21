b81c772c4183cfdd2c8243deb59afb47685f555d
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-21 19:36:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let deg = 0;
+
+let line = document.getElementById("line");
+
+setInterval(()=>{
+	deg += 2;
+	line.style.transform = `rotate(${deg}deg)`;
+	line.style.transition = 'transform 0.5s ease';
+},20)
